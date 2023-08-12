d6c191923b51d16e25d9a7a67f9703f1fa4e7656
Наталия Громова <109297130+Nataliya-miyau@users.noreply.github.com>
2023-08-12 19:11:30 +0300

    Update models.py

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -218,7 +218,7 @@ class Favorite(FavoriteShoppingCartModel):
         ]
 
 
-class Shopping_сart(FavoriteShoppingCartModel):
+class ShoppingСart(FavoriteShoppingCartModel):
 
     class Meta:
         verbose_name = 'Список покупок'
